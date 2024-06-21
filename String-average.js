6a5f61cfec0a974bc1904e32fdfa5402b2a64952
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-06-20 21:53:29 -0700

    Update String-average.js

--- String-average.js
+++ String-average.js
@@ -35,3 +35,28 @@ const averageString = (str) => {
 }
 
 // or
+
+function averageString(str) {
+  let numObj = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+  },
+  sum = 0;
+  str = str.split(' ');
+  for(let i = 0; i < str.length; i++) {
+    if(numObj[str[i]] === undefined) {
+      return 'n/a';
+    } else {
+      sum += numObj[str[i]];
+    }
+  }
+  return Object.keys(numObj)[Math.floor(sum/str.length)];
+}
