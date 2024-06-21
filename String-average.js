d2a3aa796bcd862b1b1aad7e087f5d1f73996b46
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-06-20 21:52:43 -0700

    Update String-average.js

--- String-average.js
+++ String-average.js
@@ -12,3 +12,24 @@ If the string is empty or includes a number greater than 9, return "n/a"
 
 
 // Solution
+
+const numbers = [
+  'zero',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+];
+
+const averageString = (str) => {
+  const nums = str.split(' ').map(v => numbers.indexOf(v));
+  if (nums.includes(-1)) return 'n/a'
+  const sum = nums.reduce((prev, cur) => prev + cur);
+  const average = Math.floor(sum / nums.length)
+  return numbers[average]
+}
